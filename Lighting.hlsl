df640ddd00ca60a29c5f9856d2247d99eeb2b56a
EclipseFlower <eclipseflowernju@gmail.com>
2020-12-01 14:31:34 +0800

    lighting

---
+++ Lighting.hlsl
@@ -0,0 +1,5 @@
+float4 ComputeLighting(Light gLights[16], Material mat,
+	float3 pos, float3 normal, float3 toEye)
+{
+
+}
--- color.hlsl
+++ color.hlsl
@@ -4,6 +4,8 @@
 // Transforms and colors geometry.
 //***************************************************************************************
 
+#include "Lighting.hlsl"
+
 struct Light
 {
 	int type;
@@ -15,26 +17,48 @@ struct Light
 	float spotPower;
 };
 
+struct Material
+{
+	float4 diffuseAlbedo;
+	float3 fresnelR0;
+	float shininess;
+};
+
 cbuffer cbPerObject : register(b0)
 {
+	float4x4 gWorld;
+	float4x4 gInvWorld;
 	float4x4 gWorldViewProj; 
 };
 
 cbuffer cbPerPass : register(b1)
 {
+	float3 gEyePosW;
+	int gLightCount;
+	float4 gAmbientLight;
 	Light gLights[16];
+};
+
+cbuffer cbPerMaterial : register(b2)
+{
+	float4 diffuseAlbedo;
+	float3 fresnelR0;
+	float shininess;
 };
 
 struct VertexIn
 {
-	float3 PosL  : POSITION;
-    float4 Color : COLOR;
+	float3 PosL    : POSITION;
+	float3 NormalL : NORMAL;
+    float4 Color   : COLOR;
 };
 
 struct VertexOut
 {
-	float4 PosH  : SV_POSITION;
-    float4 Color : COLOR;
+	float4 PosH    : SV_POSITION;
+	float3 PosW    : POSITION;
+	float3 NormalW : NORMAL;
+    float4 Color   : COLOR;
 };
 
 VertexOut VS(VertexIn vin)
@@ -43,7 +67,9 @@ VertexOut VS(VertexIn vin)
 	
 	// Transform to homogeneous clip space.
 	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
-	
+	float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+	vout.PosW = posW.xyz;
+	vout.NormalW = mul(vin.NormalL, transpose(gInvWorld));
 	// Just pass vertex color into the pixel shader.
     vout.Color = vin.Color;
     
@@ -52,7 +78,19 @@ VertexOut VS(VertexIn vin)
 
 float4 PS(VertexOut pin) : SV_Target
 {
-    return pin.Color;
+	float4 ambient = gAmbientLight * diffuseAlbedo;
+
+	pin.NormalW = normalize(pin.NormalW);
+	Material mat;
+	mat.diffuseAlbedo = diffuseAlbedo;
+	mat.fresnelR0 = fresnelR0;
+	mat.shininess = shininess;
+	float3 toEyeW = normalize(gEyePosW - pin.PosW);
+
+	float4 directLight = ComputeLighting(gLights, mat, pin.PosW,
+		pin.NormalW, toEyeW);
+	float4 litColor = ambient + directLight;
+    return float4(litColor.rgb, diffuseAlbedo.a);
 }
 
 
