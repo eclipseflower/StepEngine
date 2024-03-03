a3549e2a5d8cb4f05537eacffb80ae3b2a71ba5d
EclipseFlower <eclipseflowernju@gmail.com>
2024-03-03 19:08:13 +0800

    step 1

--- Launch/Windows/LaunchWindows.cpp
+++ Launch/Windows/LaunchWindows.cpp
@@ -1,5 +1,10 @@
 #include <windows.h>
 
+int LaunchWindowsStartup(HINSTANCE hInstance)
+{
+
+}
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ char* pCmdLine, _In_ int nCmdShow)
 {
     int result = LaunchWindowsStartup(hInstance);
