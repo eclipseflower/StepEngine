023023f753eeae19eeb4060021aa90e1754c41f7
eclipseflower <eclipseflowernju@gmail.com>
2024-05-21 15:01:28 +0800

    step4 vector

--- Core/Math/Vector.h
+++ Core/Math/Vector.h
@@ -1,6 +1,8 @@
 #ifndef _VECTOR_H_
 #define _VECTOR_H_
 
+#define VECTOR_EPSILON (1.e-4f)
+
 namespace Step
 {
     template<typename T>
@@ -34,12 +36,26 @@ namespace Step
 
         inline Vector<T> operator+ (const Vector<T>& v) const;
         inline Vector<T> operator- (const Vector<T>& v) const;
+        inline Vector<T> operator* (const Vector<T>& v) const;
+        inline Vector<T> operator/ (const Vector<T>& v) const;
+
+        inline bool operator== (const Vector<T>& v) const;
+        inline bool operator!= (const Vector<T>& v) const;
 
         template<typename ARGTYPE>
         inline Vector<T> operator* (ARGTYPE scale) const requires std::is_arithmetic_v<T>
         {
             return Vector<T>(x * static_cast<T>(scale), y * static_cast<T>(scale), z * static_cast<T>(scale));
         }
+
+        template<typename ARGTYPE>
+        inline Vector<T> operator/ (ARGTYPE scale) const requires std::is_arithmetic_v<T>
+        {
+            const T rScale = 1 / static_cast<T>(scale);
+            return Vector<T>(x / rScale, y / rScale, z / rScale);
+        }
+
+        inline bool Equals(const Vector<T>& v, const T tolerance = VECTOR_EPSILON) const;
 
         inline static Vector<T> Cross(const Vector<T>& lhs, const Vector<T>& rhs);
         inline static T Dot(const Vector<T>& lhs, const Vector<T>& rhs);
@@ -58,7 +74,37 @@ namespace Step
     }
 
     template<typename T>
-    inline Vector<T> Cross(const Vector<T>& lhs, const Vector<T>& rhs)
+    inline Vector<T> Vector<T>::operator*(const Vector<T>& v) const
+    {
+        return Vector<T>(x * v.x, y * v.y, z * v.z);
+    }
+
+    template<typename T>
+    inline Vector<T> Vector<T>::operator/(const Vector<T>& v) const
+    {
+        return Vector<T>(x / v.x, y / v.y, z / v.z);
+    }
+
+    template<typename T>
+    inline bool Vector<T>::operator==(const Vector<T>& v) const
+    {
+        return x == v.x && y == v.y && z == v.z;
+    }
+
+    template<typename T>
+    inline bool Vector<T>::operator!=(const Vector<T>& v) const
+    {
+        return x != v.x || y != v.y || z != v.z;
+    }
+
+    template<typename T>
+    inline bool Vector<T>::Equals(const Vector<T>& v, const T tolerance) const
+    {
+        return std::abs(x - v.x) <= tolerance && std::abs(y - v.y) <= tolerance && std::abs(z - v.z) <= tolerance;
+    }
+
+    template<typename T>
+    inline Vector<T> Vector<T>::Cross(const Vector<T>& lhs, const Vector<T>& rhs)
     {
         return Vector<T>
         {
@@ -69,7 +115,7 @@ namespace Step
     }
 
     template<typename T>
-    inline T Dot(const Vector<T>& lhs, const Vector<T>& rhs)
+    inline T Vector<T>::Dot(const Vector<T>& lhs, const Vector<T>& rhs)
     {
         return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
     }
