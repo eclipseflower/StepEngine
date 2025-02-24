f036c1482e89d4a2a08141f94f46eb6154e05b9c
eclipseflower <eclipseflowernju@gmail.com>
2025-02-24 21:02:34 +0800

    add quat function

--- Source/Runtime/Math/Quaternion.cpp
+++ Source/Runtime/Math/Quaternion.cpp
@@ -2,6 +2,15 @@
 
 namespace StepEngine
 {
+    Quaternion::Quaternion(Matrix4x4 m)
+    {
+        float trace = m.Get(0, 0) + m.Get(1, 1) + m.Get(2, 2);
+        if (trace > 0.0f)
+        {
+            w = sqrt(trace + 1.0f) / 2.0f;
+        }
+    }
+
     Vector3 Quaternion::Rotate(const Vector3& v) const
     {
         Vector3 q(x, y, z);
@@ -11,6 +20,47 @@ namespace StepEngine
 
     Matrix4x4 Quaternion::ToMatrix() const
     {
+        const float x2 = x * 2.0f;
+        const float y2 = y * 2.0f;
+        const float z2 = z * 2.0f;
 
+        const float xx = x * x2;
+        const float xy = x * y2;
+        const float xz = x * z2;
+
+        const float yx = y * x2;
+        const float yy = y * y2;
+        const float yz = y * z2;
+
+        const float zx = z * x2;
+        const float zy = z * y2;
+        const float zz = z * z2;
+
+        const float wx = w * x2;
+        const float wy = w * y2;
+        const float wz = w * z2;
+
+        Matrix4x4 m;
+        m.Get(0, 0) = 1.0f - (yy + zz);
+        m.Get(1, 0) = xy + wz;
+        m.Get(2, 0) = xz - wy;
+        m.Get(3, 0) = 0.0f;
+
+        m.Get(0, 1) = xy - wz;
+        m.Get(1, 1) = 1.0f - (xx + zz);
+        m.Get(2, 1) = yz + wx;
+        m.Get(3, 1) = 0.0f;
+
+        m.Get(0, 2) = xz + wy;
+        m.Get(1, 2) = yz - wx;
+        m.Get(2, 2) = 1.0f - (xx + yy);
+        m.Get(3, 2) = 0.0f;
+
+        m.Get(0, 3) = 0.0f;
+        m.Get(1, 3) = 0.0f;
+        m.Get(2, 3) = 0.0f;
+        m.Get(3, 3) = 1.0f;
+
+        return m;
     }
 }
--- Source/Runtime/Math/Quaternion.h
+++ Source/Runtime/Math/Quaternion.h
@@ -10,6 +10,7 @@ namespace StepEngine
         float x, y, z, w;
 
         Quaternion(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+        Quaternion(Matrix4x4 m);
 
         Vector3 Rotate(const Vector3& v) const;
 
